26a51333df607abfeb4f6c75e19773890b38e06d
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-09 05:50:13 -0300

    Segundo Desafio parte1

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,49 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface PropsSideBar {
+  selectedGenreId: number;
+  onClickButton(id: number): void;
+}
+
+export function SideBar(props: PropsSideBar) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const { selectedGenreId, onClickButton } = props;
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+
+  return (
+    <>
+       <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map((genre : GenreResponseProps) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+    </>
+   
+  );
 }
